2f25685717e0852af95b55ffc3d7306fff18cb0d
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2023-10-07 15:02:01 -0400

    Add files via upload

--- src/main/java/com/example/JieunMotivationalQ/MainActivity.java
+++ src/main/java/com/example/JieunMotivationalQ/MainActivity.java
@@ -12,6 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapterPeople.ItemClickListener {
@@ -27,36 +33,9 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        txtTitle = findViewById(R.id.txtTitle);
-
-
-        // data to populate the RecyclerView with
-        ArrayList<String> peopleNames = new ArrayList<>();
-        peopleNames.add("Feel it!");
-        peopleNames.add("Woman Power");
-        peopleNames.add("Gary Vaynerchuk");
-        peopleNames.add("Arnold Schwarzenegger");
-        peopleNames.add("Confucius");
-        peopleNames.add("Albert Einstein");
-
-        // data to populate the RecyclerView with
-        ArrayList<String> peoplePictures = new ArrayList<>();
-        peoplePictures.add("feel_it");
-        peoplePictures.add("woman_power");
-        peoplePictures.add("gary_vaynerchuk");
-        peoplePictures.add("arnold_schwarzenegger");
-        peoplePictures.add("confucius");
-        peoplePictures.add("albert_einstein");
-
-        // set up the RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MyRecyclerViewAdapterPeople(this, peopleNames, peoplePictures);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
-
+        FirebaseApp.initializeApp(this);
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
 
         // Creates a button that mimics a crash when pressed
         Button crashButton = new Button(this);
@@ -71,6 +50,44 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT));
 
+        // Ad initialize
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                // AdMob initialization is complete.
+            }
+        });
+
+
+        setContentView(R.layout.activity_main);
+
+        txtTitle = findViewById(R.id.txtTitle);
+
+
+        // data to populate the RecyclerView with
+        ArrayList<String> peopleNames = new ArrayList<>();
+        peopleNames.add("Feel it!");
+        peopleNames.add("Woman Power");
+        peopleNames.add("Confucius");
+        peopleNames.add("Albert Einstein");
+        peopleNames.add("Gary Vaynerchuk");
+        peopleNames.add("Arnold Schwarzenegger");
+
+        // data to populate the RecyclerView with
+        ArrayList<String> peoplePictures = new ArrayList<>();
+        peoplePictures.add("feel_it");
+        peoplePictures.add("woman_power");
+        peoplePictures.add("confucius");
+        peoplePictures.add("albert_einstein");
+        peoplePictures.add("gary_vaynerchuk");
+        peoplePictures.add("arnold_schwarzenegger");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapterPeople(this, peopleNames, peoplePictures);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
 
     }
 
--- src/main/java/com/example/JieunMotivationalQ/MyRecyclerViewAdapterQuotes.java
+++ src/main/java/com/example/JieunMotivationalQ/MyRecyclerViewAdapterQuotes.java
@@ -1,8 +1,6 @@
 package com.example.JieunMotivationalQ;
 
-
 import android.content.Context;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,69 +10,74 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import java.util.List;
 
-public class MyRecyclerViewAdapterQuotes extends RecyclerView.Adapter<MyRecyclerViewAdapterQuotes.ViewHolder> {
+public class MyRecyclerViewAdapterQuotes extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private int selectedQuoteIndex = -1; // Initialize with an invalid index
 
     private final List<String> mData;
     private final LayoutInflater mInflater;
     private ItemClickListener mClickListener;
+    private final AdRequest adRequest;
 
-//    Todo continue adding admob
-    private AdView mAdView;
-    protected void onCreate(Bundle savedInstanceState) {
-
-        MobileAds.initialize(mAdView.getContext(), new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-
-        mAdView = mAdView.findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-    }
-//    End of ad
+    private static final int VIEW_TYPE_QUOTE = 0;
+    private static final int VIEW_TYPE_AD = 1;
 
     // data is passed into the constructor
     MyRecyclerViewAdapterQuotes(Context context, List<String> data) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
+        this.adRequest = new AdRequest.Builder().build();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0 && selectedQuoteIndex >= 0 && selectedQuoteIndex < mData.size()) {
+            return VIEW_TYPE_AD;
+        } else {
+            return VIEW_TYPE_QUOTE;
+        }
     }
 
     // inflates the row layout from xml when needed
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.cardview_quotes, parent, false);
-        return new ViewHolder(view);
-    }
-
-
-
-    // binds the data to the TextView
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        String quote = mData.get(position);
-
-        // Check if the current position is the selected quote index
-        if (position == 0 && selectedQuoteIndex >= 0 && selectedQuoteIndex < mData.size()) {
-            holder.myTextView.setText(mData.get(selectedQuoteIndex));
-            holder.myTextView.setVisibility(View.VISIBLE);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_QUOTE) {
+            View view = mInflater.inflate(R.layout.cardview_quotes, parent, false);
+            return new QuoteViewHolder(view);
         } else {
-            holder.myTextView.setText(quote);
-            holder.myTextView.setVisibility(
-                    (selectedQuoteIndex >= 0 && selectedQuoteIndex < mData.size() && position != 0)
-                            ? View.GONE
-                            : View.VISIBLE
-            );
+            View adView = mInflater.inflate(R.layout.ad_view_item, parent, false);
+            return new AdViewHolder(adView);
         }
     }
 
+    // binds the data to the TextView
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof QuoteViewHolder) {
+            QuoteViewHolder quoteViewHolder = (QuoteViewHolder) holder;
+            String quote = mData.get(position);
+
+            // Check if the current position is the selected quote index
+            if (position == 0 && selectedQuoteIndex >= 0 && selectedQuoteIndex < mData.size()) {
+                quoteViewHolder.myTextView.setText(mData.get(selectedQuoteIndex));
+                quoteViewHolder.myTextView.setVisibility(View.VISIBLE);
+            } else {
+                quoteViewHolder.myTextView.setText(quote);
+                quoteViewHolder.myTextView.setVisibility(
+                        (selectedQuoteIndex >= 0 && selectedQuoteIndex < mData.size() && position != 0)
+                                ? View.GONE
+                                : View.VISIBLE
+                );
+            }
+        } else if (holder instanceof AdViewHolder) {
+            // Handle the AdViewHolder
+            AdViewHolder adViewHolder = (AdViewHolder) holder;
+            AdView adView = adViewHolder.mAdView;
+            adView.loadAd(adRequest);
+        }
+    }
 
     // total number of rows
     @Override
@@ -82,20 +85,23 @@ public class MyRecyclerViewAdapterQuotes extends RecyclerView.Adapter<MyRecycler
         return mData.size();
     }
 
-
-    // stores and recycles views as they are scrolled off screen
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    // ViewHolder for quotes
+    private static class QuoteViewHolder extends RecyclerView.ViewHolder {
         TextView myTextView;
 
-        ViewHolder(View itemView) {
+        QuoteViewHolder(View itemView) {
             super(itemView);
             myTextView = itemView.findViewById(R.id.personName);
-            itemView.setOnClickListener(this);
         }
+    }
 
-        @Override
-        public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+    // ViewHolder for ads
+    private static class AdViewHolder extends RecyclerView.ViewHolder {
+        AdView mAdView;
+
+        AdViewHolder(View itemView) {
+            super(itemView);
+            mAdView = itemView.findViewById(R.id.adView);
         }
     }
 
@@ -114,7 +120,6 @@ public class MyRecyclerViewAdapterQuotes extends RecyclerView.Adapter<MyRecycler
         void onItemClick(View view, int position);
     }
 
-
     // Setter method to set the selected quote index
     public void setSelectedQuoteIndex(int index) {
         selectedQuoteIndex = index;
@@ -129,3 +134,146 @@ public class MyRecyclerViewAdapterQuotes extends RecyclerView.Adapter<MyRecycler
         notifyDataSetChanged();
     }
 }
+
+//package com.example.JieunMotivationalQ;
+//
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import com.google.android.gms.ads.AdRequest;
+//import com.google.android.gms.ads.AdView;
+//
+//import java.util.List;
+//
+//public class MyRecyclerViewAdapterQuotes extends RecyclerView.Adapter<MyRecyclerViewAdapterQuotes.ViewHolder> {
+//    private int selectedQuoteIndex = -1; // Initialize with an invalid index
+//
+//    private final List<String> mData;
+//    private final LayoutInflater mInflater;
+//    private ItemClickListener mClickListener;
+//    private final AdRequest adRequest;
+//
+//    private static final int VIEW_TYPE_QUOTE = 0;
+//    private static final int VIEW_TYPE_AD = 1;
+//
+//    // data is passed into the constructor
+//    MyRecyclerViewAdapterQuotes(Context context, List<String> data) {
+//        this.mInflater = LayoutInflater.from(context);
+//        this.mData = data;
+//        this.adRequest = new AdRequest.Builder().build();
+//
+//    }
+//
+//
+//    // inflates the row layout from xml when needed
+//    @Override
+//    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View view = mInflater.inflate(R.layout.cardview_quotes, parent, false);
+//        return new ViewHolder(view);
+//
+//    }
+//
+//
+//    // binds the data to the TextView
+//    @Override
+//    public void onBindViewHolder(ViewHolder holder, int position) {
+//        String quote = mData.get(position);
+//
+//
+//        // Check if the current position is the selected quote index
+//        if (position == 0 && selectedQuoteIndex >= 0 && selectedQuoteIndex < mData.size()) {
+//            holder.myTextView.setText(mData.get(selectedQuoteIndex));
+//            holder.myTextView.setVisibility(View.VISIBLE);
+//
+//        } else {
+//            holder.myTextView.setText(quote);
+//            holder.myTextView.setVisibility(
+//                    (selectedQuoteIndex >= 0 && selectedQuoteIndex < mData.size() && position != 0)
+//                            ? View.GONE
+//                            : View.VISIBLE
+//            );
+//        }
+//    }
+//
+//
+//    // total number of rows
+//    @Override
+//    public int getItemCount() {
+//        return mData.size();
+//    }
+//
+//
+//    // stores and recycles views as they are scrolled off screen
+//    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+//        TextView myTextView;
+//
+//        ViewHolder(View itemView) {
+//            super(itemView);
+//            myTextView = itemView.findViewById(R.id.personName);
+//            itemView.setOnClickListener(this);
+//        }
+//
+//        @Override
+//        public void onClick(View view) {
+//            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+//        }
+//    }
+//
+//    // ViewHolder for ads
+//    private class AdViewHolder extends RecyclerView.ViewHolder {
+//        AdView mAdView;
+//
+//        AdViewHolder(View itemView) {
+//            super(itemView);
+//            mAdView = itemView.findViewById(R.id.adView);
+//        }
+//    }
+//
+//
+//    // return the appropriate view type based on the position
+//    @Override
+//    public int getItemViewType(int position) {
+//        if (position == 0 && selectedQuoteIndex >= 0 && selectedQuoteIndex < mData.size()) {
+//            return VIEW_TYPE_AD;
+//        } else {
+//            return VIEW_TYPE_QUOTE;
+//        }
+//    }
+//
+//
+//    // convenience method for getting data at click position
+//    String getItem(int id) {
+//        return mData.get(id);
+//    }
+//
+//    // allows clicks events to be caught
+//    void setClickListener(ItemClickListener itemClickListener) {
+//        this.mClickListener = itemClickListener;
+//    }
+//
+//    // parent activity will implement this method to respond to click events
+//    public interface ItemClickListener {
+//        void onItemClick(View view, int position);
+//    }
+//
+//
+//    // Setter method to set the selected quote index
+//    public void setSelectedQuoteIndex(int index) {
+//        selectedQuoteIndex = index;
+//
+//        // If a quote is selected, keep only that quote in the data list
+//        if (index >= 0 && index < mData.size()) {
+//            String selectedQuote = mData.get(index);
+//            mData.clear();
+//            mData.add(selectedQuote);
+//        }
+//
+//        notifyDataSetChanged();
+//    }
+//}
